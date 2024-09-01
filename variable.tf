50feeb1ee29da5575b548b8f1dec826e657d2e45
vaish17d <113599137+vaish17d@users.noreply.github.com>
2024-09-01 22:54:44 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,12 @@
+variable "region_name" {
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "access_key" {
+  type        = string
+}
+
+variable "secret_key" {
+  type        = string
+}
